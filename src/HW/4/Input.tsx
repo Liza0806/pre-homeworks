f25a9b0178de76de70abe3f556c56bd0103ad338
Liza0806 <126647343+Liza0806@users.noreply.github.com>
2024-04-02 12:01:54 +0300

    delete comments

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,12 +1,13 @@
 type InputPropsType = {
-	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: (newValue: string) => void;// НУЖНО ПРОТИПИЗИРОВАТЬ
+	currentText: string;
+	setCurrentText: (newValue: string) => void;
 };
 
 export const Input = (props: InputPropsType) => {
 	const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
 		props.setCurrentText(event.currentTarget.value)
 	};
+
 
 	return (
 	  <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
